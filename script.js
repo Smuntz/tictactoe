245c8c3fe0d5f1d2a32f4f7690bae14f86a204b6
eldude <eldude73@gmail.com>
2025-01-14 21:27:22 +1100

    Finished a working game

--- script.js
+++ script.js
@@ -4,25 +4,64 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-    hand = Math.floor(Math.random() * 3 );
+    hand = Math.floor(Math.random() * 3);
 
     if (hand === 0) {
         hand = "Rock";
-    } else  if (hand === 1) {
+    } else if (hand === 1) {
         hand = "Paper";
     } else {
         hand = "Scissors";
     }
-    return computerChoice = hand;
+    return computerChoice = hand.toLowerCase();
 }
 
 function getHumanChoice() {
-return humanChoice = prompt("Rock Paper Scissors?");
+    humanChoice = prompt("Rock Paper Scissors?");
+    return humanChoice.toLowerCase();
 
 }
 
-getHumanChoice();
-getComputerChoice() 
+function playRound(humanChoice, computerChoice) {
+    console.log(`Human: ${humanChoice}`);
+    console.log(`Computer: ${computerChoice}`);
+    if (humanChoice === computerChoice) {
+        console.log("Draw! Play again")        
+    } else if (humanChoice === "rock" && computerChoice == "scissors") {
+        humanScore++;
+        console.log("You WIN! Rock beasts Scissors")        
+    } else if (humanChoice === "paper" && computerChoice == "rock") {
+        humanScore++;
+        console.log("You WIN! Paper beasts Rock")        
+    } else if (humanChoice === "scissors" && computerChoice == "paper") {
+        humanScore++;
+        console.log("You WIN! Scissors beasts Paper")        
+    } else {
+        computerScore++;
+        console.log(`You Loose! ${computerChoice} beasts ${humanChoice}`)
+        
+    }
 
-console.log("Human: " + humanChoice);
-console.log("Computer: " + computerChoice);
+}
+
+function playGame() {
+
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+        console.log(`Score is You: ${humanScore} Computer: ${computerScore}`);
+        if (i < 4) {
+        alert("Next Round?");
+        }
+    }
+    if (humanScore > computerScore) {
+        alert("You Win! Play Again?")
+    } else if (humanScore < computerScore) {
+        alert("You Lose! Play again?")        
+    } else {
+        alert("Final Score: Draw! Play again")
+    }
+
+}
+playGame();
+
+
