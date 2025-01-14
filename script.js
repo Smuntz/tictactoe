7d84f89cb556b4d460fd224658d25ebb0b48a2c5
eldude <eldude73@gmail.com>
2025-01-14 22:01:30 +1100

    made sure scores reset and game replayed by updating the playGame function

--- script.js
+++ script.js
@@ -26,20 +26,20 @@ function playRound(humanChoice, computerChoice) {
     console.log(`Human: ${humanChoice}`);
     console.log(`Computer: ${computerChoice}`);
     if (humanChoice === computerChoice) {
-        console.log("Draw! Play again")        
+        console.log("Draw! Play again")
     } else if (humanChoice === "rock" && computerChoice == "scissors") {
         humanScore++;
-        console.log("You WIN! Rock beasts Scissors")        
+        console.log("You WIN! Rock beasts Scissors")
     } else if (humanChoice === "paper" && computerChoice == "rock") {
         humanScore++;
-        console.log("You WIN! Paper beasts Rock")        
+        console.log("You WIN! Paper beasts Rock")
     } else if (humanChoice === "scissors" && computerChoice == "paper") {
         humanScore++;
-        console.log("You WIN! Scissors beasts Paper")        
+        console.log("You WIN! Scissors beasts Paper")
     } else {
         computerScore++;
         console.log(`You Loose! ${computerChoice} beasts ${humanChoice}`)
-        
+
     }
 
 }
@@ -50,17 +50,19 @@ function playGame() {
         playRound(getHumanChoice(), getComputerChoice());
         console.log(`Score is You: ${humanScore} Computer: ${computerScore}`);
         if (i < 4) {
-        alert("Next Round?");
+            alert("Next Round?");
         }
     }
     if (humanScore > computerScore) {
         alert("You Win! Play Again?")
     } else if (humanScore < computerScore) {
-        alert("You Lose! Play again?")        
+        alert("You Lose! Play again?")
     } else {
         alert("Final Score: Draw! Play again")
     }
-
+    humanScore = 0;
+    computerScore = 0;
+    playGame();
 }
 playGame();
 
